82b7f711778dbe1e7b13967b7640e1c611bbbafe
rowpacmal <rowpacmaldev@gmail.com>
2025-08-13 19:30:49 +0200

    Added button to App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,15 @@
 function App() {
   return (
-    <>
+    <div className='flex flex-col items-center justify-center h-screen gap-4'>
       <h1 className='text-3xl font-bold'>Animg AI</h1>
-    </>
+
+      <button
+        className='bg-blue-500 hover:bg-blue-700 text-white py-2 px-4 rounded'
+        onClick={() => console.log('Generate image')}
+      >
+        Generate
+      </button>
+    </div>
   );
 }
 
