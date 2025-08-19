4b9fedd306544e3eaf8fe7115e629acaa2df53c4
rowpacmal <rowpacmaldev@gmail.com>
2025-08-19 15:03:04 +0200

    Backup commit

--- src/App.tsx
+++ src/App.tsx
@@ -3,9 +3,39 @@ import reactLogo from '/react.svg';
 import viteLogo from '/vite.svg';
 import './styles/App.css';
 
+import { InferenceClient } from '@huggingface/inference';
+import { useEffect, useState } from 'react';
+
 function App() {
   const count = useAppStore((state) => state.count);
   const increment = useAppStore((state) => state.increment);
+
+  const client = new InferenceClient(import.meta.env.VITE_HF_TOKEN);
+  const [img, setImg] = useState<string | null>(null);
+
+  async function test() {
+    const response = await client.textToImage({
+      model: 'black-forest-labs/FLUX.1-dev',
+      inputs: 'a picture of a green bird',
+      provider: 'fal-ai',
+    });
+    console.log(response);
+
+    const objUrl = URL.createObjectURL(response as unknown as Blob);
+    setImg(objUrl);
+
+    const img = document.createElement('img');
+    img.src = objUrl;
+    document.body.appendChild(img);
+
+    // URL.revokeObjectURL(objUrl);
+  }
+
+  useEffect(() => {
+    return () => {
+      URL.revokeObjectURL(img);
+    };
+  }, [img]);
 
   return (
     <>
@@ -32,6 +62,12 @@ function App() {
       <p className='read-the-docs'>
         Click on the Vite and React logos to learn more
       </p>
+
+      {img && <img src={img} />}
+
+      <div>
+        <button onClick={test}>Test</button>
+      </div>
     </>
   );
 }
