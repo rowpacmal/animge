1a6a96de0d98becc1085a04e91c69736644780cd
rowpacmal <rowpacmaldev@gmail.com>
2025-08-21 15:01:39 +0200

    Removed verbose python typing

--- python/run.py
+++ python/run.py
@@ -1,4 +1,3 @@
-from typing import Union
 from fastapi import FastAPI
 
 
@@ -11,5 +10,5 @@ def read_root():
 
 
 @app.get("/items/{item_id}")
-def read_item(item_id: int, q: Union[str, None] = None):
+def read_item(item_id: int, q: str | None = None):
     return {"item_id": item_id, "q": q}
