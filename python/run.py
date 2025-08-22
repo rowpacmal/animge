51356eded834143637a4ea2d6d23fca73c6bc7d5
rowpacmal <rowpacmaldev@gmail.com>
2025-08-22 23:16:41 +0200

    Updated python run.py
    Added comments, and updated `save_dir` and `file_path`

--- python/run.py
+++ python/run.py
@@ -1,17 +1,25 @@
+# Standard libraries
+import io
+from pathlib import Path
+
+# Third-party libraries
 from fastapi import FastAPI
 from fastapi.responses import StreamingResponse
-import io
-from app.schemas import PromptRequest
-from app.model import generate_image
-from pathlib import Path
 from platformdirs import user_documents_dir
 
+# Local application imports
+from app.schemas import PromptRequest
+from app.model import generate_image
 
+
+# Initialize FastAPI
 app = FastAPI()
 
 
+# API Endpoints
 @app.post("/generate")
 def generate(req: PromptRequest):
+    # Generate image from user request
     image, used_seed = generate_image(
         prompt=req.prompt,
         negative_prompt=req.negative_prompt,
@@ -22,11 +30,13 @@ def generate(req: PromptRequest):
         seed=req.seed,
     )
 
+    # Save temp image locally in the user's documents folder
     documents = Path(user_documents_dir())
-    save_dir = documents / "MyApp"
+    save_dir = documents / "Animge" / "temp"
     save_dir.mkdir(parents=True, exist_ok=True)
 
-    file_path = save_dir / f"generated_{used_seed}.png"
+    file_path = save_dir / f"temp_{used_seed}.png"
     image.save(file_path, format="PNG")
 
+    # Return temp image path and used seed
     return {"path": str(file_path), "seed": used_seed}
