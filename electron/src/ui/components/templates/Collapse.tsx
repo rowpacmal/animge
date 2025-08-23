1cfcd93ea666325ad2d02c3624bc04e7397bee8d
rowpacmal <rowpacmaldev@gmail.com>
2025-08-23 20:21:31 +0200

    Added ui component template Collapse.tsx
    Refactored repetitive ui code into a dedicated component

---
+++ electron/src/ui/components/templates/Collapse.tsx
@@ -0,0 +1,35 @@
+import { useState } from 'react';
+
+export function Collapse({
+  children,
+  icon,
+  title,
+}: {
+  children: React.ReactNode;
+  icon: React.ReactNode;
+  title: string;
+}) {
+  const [isOpen, setIsOpen] = useState(false);
+
+  return (
+    <div className="max-w-lg mx-auto">
+      <div className="collapse collapse-arrow bg-base-100 border-base-300 border">
+        <input
+          type="checkbox"
+          checked={isOpen}
+          onChange={() => setIsOpen(!isOpen)}
+        />
+
+        <div className="collapse-title font-semibold flex items-center gap-2 text-primary">
+          {icon} {title}
+        </div>
+
+        <div className="collapse-content text-sm">
+          <div className="flex flex-col gap-2 overflow-y-auto max-h-[560px] p-1">
+            {children}
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
