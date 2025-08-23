9a44256b1e4753843b8b0b9104f21ae09a9af444
rowpacmal <rowpacmaldev@gmail.com>
2025-08-23 20:24:45 +0200

    Updated ui AppMain.tsx

--- electron/src/ui/layout/AppMain.tsx
+++ electron/src/ui/layout/AppMain.tsx
@@ -1,17 +1,33 @@
+import { IconPhotoFilled } from '@tabler/icons-react';
 import { AdvanceSettings, UserTasks } from '../components';
 import styles from './AppMain.module.css';
 
 export function AppMain() {
   return (
     <main className={`${styles.main} ${styles['dotted-background']}`}>
-      <UserTasks />
+      <div className="order-1">
+        <UserTasks />
+      </div>
 
-      <textarea
-        className="textarea mx-auto resize-none h-32 w-full max-w-lg xl:max-w-none order-3 xl:order-2"
-        placeholder="You can generate images by entering prompts here."
-      ></textarea>
+      <div className="flex flex-col gap-4 w-full max-w-lg mx-auto xl:max-w-none order-3 xl:order-2">
+        <textarea
+          className="textarea resize-none h-32 w-full"
+          placeholder="You can generate images by entering prompts here."
+        ></textarea>
 
-      <AdvanceSettings />
+        <div className="flex justify-between">
+          <div></div>
+
+          <button className="flex gap-4 btn btn-secondary">
+            <IconPhotoFilled />
+            <span>Generate Image</span>
+          </button>
+        </div>
+      </div>
+
+      <div className="order-2 xl:order-3">
+        <AdvanceSettings />
+      </div>
     </main>
   );
 }
