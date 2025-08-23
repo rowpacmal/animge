33fa113b518462368eaab57061090a407116f542
rowpacmal <rowpacmaldev@gmail.com>
2025-08-23 20:18:58 +0200

    Added ui theme button to AppHeader.tsx

--- electron/src/ui/layout/AppHeader.tsx
+++ electron/src/ui/layout/AppHeader.tsx
@@ -1,7 +1,21 @@
-import { IconSettings, IconSparkles } from '@tabler/icons-react';
+import {
+  IconMoon,
+  IconSettings,
+  IconSparkles,
+  IconSun,
+} from '@tabler/icons-react';
 import styles from './AppHeader.module.css';
+import { useState } from 'react';
 
 export function AppHeader() {
+  const [theme, setTheme] = useState(
+    document.documentElement.dataset.theme
+      ? document.documentElement.dataset.theme === 'light'
+        ? 'light'
+        : 'dark'
+      : 'dark'
+  );
+
   return (
     <header className={styles.header}>
       <div className={styles['h-container']}>
@@ -13,8 +27,24 @@ export function AppHeader() {
       </div>
 
       <div className={styles['button-container']}>
+        <button
+          className={styles.button}
+          onClick={() => {
+            // Change data-theme
+            if (document.documentElement.dataset.theme === 'light') {
+              document.documentElement.dataset.theme = 'dark';
+              setTheme('dark');
+            } else {
+              document.documentElement.dataset.theme = 'light';
+              setTheme('light');
+            }
+          }}
+        >
+          {theme === 'light' ? <IconSun /> : <IconMoon />}
+        </button>
+
         <button className={styles.button}>
-          <IconSettings size={24} />
+          <IconSettings />
         </button>
       </div>
     </header>
