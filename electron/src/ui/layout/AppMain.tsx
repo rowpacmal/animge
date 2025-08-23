34a3117065c24ce2f6d9c500203f33ae59e7ce26
rowpacmal <rowpacmaldev@gmail.com>
2025-08-23 19:40:36 +0200

    Updated ui AppMain.tsx and its styles

--- electron/src/ui/layout/AppMain.tsx
+++ electron/src/ui/layout/AppMain.tsx
@@ -1,9 +1,17 @@
+import { AdvanceSettings, UserTasks } from '../components';
 import styles from './AppMain.module.css';
 
 export function AppMain() {
   return (
     <main className={`${styles.main} ${styles['dotted-background']}`}>
-      Main
+      <UserTasks />
+
+      <textarea
+        className="textarea mx-auto resize-none h-32 w-full max-w-lg xl:max-w-none order-3 xl:order-2"
+        placeholder="You can generate images by entering prompts here."
+      ></textarea>
+
+      <AdvanceSettings />
     </main>
   );
 }
