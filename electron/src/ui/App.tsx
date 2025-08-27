79ac3fc33f3fc21f4de5b248b3bc74a2c1734fbd
rowpacmal <rowpacmaldev@gmail.com>
2025-08-27 14:17:27 +0200

    Added ui download of model on app startup

--- electron/src/ui/App.tsx
+++ electron/src/ui/App.tsx
@@ -1,17 +1,23 @@
 import { useEffect, useState } from 'react';
 import { AppFooter, AppHeader, AppMain } from './layout';
-import { IconDownload } from '@tabler/icons-react';
+import { IconDownload, IconError404 } from '@tabler/icons-react';
 
 function App() {
   const [isDownloading, setIsDownloading] = useState(true);
+  const [isError, setIsError] = useState(false);
 
   useEffect(() => {
     const evtSource = new EventSource(
       'http://localhost:8000/api/v1/downloads/model'
     );
     evtSource.onmessage = (event) => {
-      if (event.data === 'DONE') {
+      if (event.data.startsWith('DONE')) {
         setIsDownloading(false);
+        setIsError(false);
+        evtSource.close();
+      } else if (event.data.startsWith('ERROR')) {
+        setIsDownloading(false);
+        setIsError(true);
         evtSource.close();
       }
     };
@@ -31,9 +37,20 @@ function App() {
         </div>
       ) : (
         <>
-          <AppHeader />
-          <AppMain />
-          <AppFooter />
+          {isError ? (
+            <div className="flex flex-col justify-center items-center gap-4 h-full w-full">
+              <div role="alert" className="alert alert-error">
+                <IconError404 />
+                <span>Failed to download resources</span>
+              </div>
+            </div>
+          ) : (
+            <>
+              <AppHeader />
+              <AppMain />
+              <AppFooter />
+            </>
+          )}
         </>
       )}
     </>
