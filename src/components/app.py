d978467e0a3e25773f6e3c802e9d8101dde86e98
rowpacmal <rowpacmaldev@gmail.com>
2025-09-02 11:16:01 +0200

    Refactored main.py and app.py

--- src/components/app.py
+++ src/components/app.py
@@ -5,29 +5,18 @@ import flet as ft
 from layouts import AppFooter, AppHeader, AppMain
 
 
-class App:
-    def __init__(self, page: ft.Page):
-        self.page = page
-
-        # Page
-        self.page.title = "Animagine"
-        self.page.theme_mode = ft.ThemeMode.DARK
+class App(ft.Container):
+    def __init__(self):
+        super().__init__()
 
         # Styles
-        self.page.padding = 0
-
-        # Window
-        self.page.window.maximized = True
-        self.page.window.min_width = 1024
-        self.page.window.min_height = 768
-        self.page.window.center()
+        self.alignment = ft.alignment.center
+        self.expand = True
 
         # UI
-        self.page.add(
-            ft.Column(
-                controls=[AppHeader(), AppMain(), AppFooter()],
-                alignment=ft.MainAxisAlignment.CENTER,
-                horizontal_alignment=ft.CrossAxisAlignment.CENTER,
-                expand=True,
-            )
+        self.content = ft.Column(
+            controls=[AppHeader(), AppMain(), AppFooter()],
+            alignment=ft.MainAxisAlignment.CENTER,
+            horizontal_alignment=ft.CrossAxisAlignment.CENTER,
+            expand=True,
         )
--- src/main.py
+++ src/main.py
@@ -4,7 +4,20 @@ from components import App
 
 
 def main(page: ft.Page):
-    App(page)
+    # Page
+    page.title = "Animge"
+
+    # Window
+    page.window.maximized = True
+    page.window.min_width = 1024
+    page.window.min_height = 768
+    page.window.center()
+
+    # Styles
+    page.padding = 0
+
+    # UI
+    page.add(App())
 
 
 ft.app(main)
