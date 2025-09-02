d5f0766201e0418698856743054222a3c9d7ffa1
rowpacmal <rowpacmaldev@gmail.com>
2025-09-02 09:48:15 +0200

    Updated runflet.bat

--- runflet.bat
+++ runflet.bat
@@ -1,6 +1,6 @@
 @echo off
 
 REM Run Flet app with hot reload and ignore cache directories
-flet run -p=50125 --name=animge -d -r -w --ignore-dirs=components/ui/__pycache__,constants/__pycache__,contexts/__pycache__,utils/__pycache__
+flet run -p=50125 --name=animge -d -r -w --ignore-dirs=components/__pycache__,components/ui/__pycache__,constants/__pycache__,contexts/__pycache__,utils/__pycache__
 
 pause
