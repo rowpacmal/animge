18ecd0a1f82b3896d6ef4a28b515de6854664a36
rowpacmal <rowpacmaldev@gmail.com>
2025-09-02 10:59:25 +0200

    Updated progress_bar.py

--- src/components/ui/progress_bar.py
+++ src/components/ui/progress_bar.py
@@ -4,6 +4,11 @@ import flet as ft
 class ProgressBar(ft.ProgressBar):
     def __init__(self, value=0):
         super().__init__()
-        self.value = value
-        self.border_radius = 20
+
+        # Styles
         self.height = 20
+        self.width = 600
+        self.border_radius = 20
+
+        # States
+        self.value = value
